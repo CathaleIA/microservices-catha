9ba6d3b7cf2a98176939482060294255ede75a25
cathaleia <eficiencia.desarrollo@copowe.com.co>
2025-05-07 11:36:13 -0500

    segundo dashboard id

--- quicksight/generateEmbedUrl/index.mjs
+++ quicksight/generateEmbedUrl/index.mjs
@@ -7,13 +7,14 @@ const CONFIG = {
   AWS_ACCOUNT_ID: "240435918890",
   QUICKSIGHT_USER_ARN: "arn:aws:quicksight:us-east-1:240435918890:user/default/diego-dev",
   ALLOWED_DOMAIN: "https://d19kpussj440vd.cloudfront.net",
-  DASHBOARD_ID: "57aab648-7a18-4f91-9c8a-0d89ffb98823"
+  // DASHBOARD_ID: "57aab648-7a18-4f91-9c8a-0d89ffb98823"
+  DASHBOARD_ID: "34c6f29e-da26-4c46-93e9-afe512a5af78"
 };
 
 export const handler = async () => { // No necesitas el parámetro 'event'
   try {
     console.log("Iniciando generación de URL...");
-    
+
     const params = {
       AwsAccountId: CONFIG.AWS_ACCOUNT_ID,
       UserArn: CONFIG.QUICKSIGHT_USER_ARN,
@@ -27,10 +28,10 @@ export const handler = async () => { // No necesitas el parámetro 'event'
     };
 
     console.log("Parámetros:", JSON.stringify(params, null, 2));
-    
+
     const command = new GenerateEmbedUrlForRegisteredUserCommand(params);
     const response = await quicksightClient.send(command);
-    
+
     console.log("Respuesta de QuickSight:", response);
 
     return {
@@ -40,8 +41,8 @@ export const handler = async () => { // No necesitas el parámetro 'event'
         "Access-Control-Allow-Origin": CONFIG.ALLOWED_DOMAIN,
         "Access-Control-Allow-Methods": "GET"
       },
-      body: JSON.stringify({ 
-        embedUrl: response.EmbedUrl 
+      body: JSON.stringify({
+        embedUrl: response.EmbedUrl
       })
     };
 
